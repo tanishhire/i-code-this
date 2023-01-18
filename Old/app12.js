7106fa4208b86311bdc72906bcf6f25938b2509b
Tanish Hire <83215038+tanishhire@users.noreply.github.com>
2023-01-18 11:43:53 +0530

    new

---
+++ Old/app12.js
@@ -0,0 +1,13 @@
+var divItems = document.getElementsByClassName("svgwidth");
+
+const selected = () => {
+  this.clear();
+  item.style.backgroundColor = "#11182e";
+};
+
+const clear = () => {
+  for (let i = 0; i < divItems.length; i++) {
+    var item = divItems[i];
+    item.style.backgroundColor = "white";
+  }
+};
